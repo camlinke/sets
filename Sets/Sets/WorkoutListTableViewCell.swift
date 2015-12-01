94ddfbfdb33273696c9666277034f70d1ff3fdc7
Cam Linke <cam.linke@gmail.com>
2015-11-30 20:19:34 -0700

    got main workout list going and uinavigationbar in place

---
+++ Sets/Sets/WorkoutListTableViewCell.swift
@@ -0,0 +1,46 @@
+//
+//  WorkoutListTableViewCell.swift
+//  Sets
+//
+//  Created by Cam Linke on 2015-11-28.
+//  Copyright © 2015 camlinke. All rights reserved.
+//
+
+import UIKit
+
+class WorkoutListTableViewCell: UITableViewCell {
+    
+    var workoutName: UILabel!
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        workoutName = UILabel(frame: CGRect(x: self.frame.size.width * 0.05,
+                                            y: (self.frame.size.height + 5) / 2,
+                                        width: self.frame.size.width * 0.72,
+                                        height: 27))
+//        workoutName.backgroundColor = UIColor.redColor()
+        workoutName.font = UIFont.systemFontOfSize(27, weight: UIFontWeightLight)
+        workoutName.text = "Upper Body Day 1"
+        contentView.addSubview(workoutName)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+        
+    }
+
+}
---
+++ Sets/Sets/WorkoutListTableViewController.swift
@@ -0,0 +1,109 @@
+//
+//  WorkoutListTableViewController.swift
+//  Sets
+//
+//  Created by Cam Linke on 2015-11-28.
+//  Copyright © 2015 camlinke. All rights reserved.
+//
+
+import UIKit
+
+class WorkoutListTableViewController: UITableViewController{
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        tableView.rowHeight = 76
+        tableView.estimatedRowHeight = 76
+        
+        // Setup Navigation Bar
+        let navigationBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 64))
+        navigationBar.backgroundColor = UIColor(red: 113, green: 46, blue: 89, alpha: 1.0)
+        
+        let navigationItem = UINavigationItem()
+        navigationItem.title = "Title"
+        navigationBar.items = [navigationItem]
+        
+        self.view.addSubview(navigationBar)
+        
+        self.tableView.registerClass(WorkoutListTableViewCell.self, forCellReuseIdentifier: "WorkoutListCell")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 100
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("WorkoutListCell") as! WorkoutListTableViewCell
+        
+        cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+        cell.accessoryType = UITableViewCellAccessoryType.DetailDisclosureButton
+        
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
